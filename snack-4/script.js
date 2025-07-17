a30972f637814147fe4ab3c6e534e694b73e5dea
omarseddati16 <omarseddati16@gmail.com>
2025-07-17 13:32:41 +0200

    fourth request

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+const newPosts = posts.map(function (element) {
+  return element.author
+});
+
+console.log(newPosts)
