0ca8125d0be552420537abe8b7f93d183267c15b
omarseddati16 <omarseddati16@gmail.com>
2025-07-17 13:35:45 +0200

    fifth request

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const newNums = nums.filter(function (num) {
+  if (num % 2 === 0)
+    return num
+});
+
+console.log(newNums)
